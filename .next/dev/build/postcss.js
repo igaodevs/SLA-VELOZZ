0ffe9c0aeadbf6a9331d8b517f94ecc991abd15f
Igor Costa Oliveira <70361857+igaodevs@users.noreply.github.com>
2025-11-21 16:16:11 -0300

    4

--- .next/dev/build/postcss.js
+++ .next/dev/build/postcss.js
@@ -1,5 +1,5 @@
 var R=require("./chunks/[turbopack]_runtime.js")("postcss.js")
-R.c("chunks/[turbopack-node]_transforms_postcss_ts_b9a3f850._.js")
+R.c("chunks/[turbopack-node]_transforms_postcss_ts_03d2e1ab._.js")
 R.c("chunks/[root-of-the-server]__7d8be06b._.js")
 R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
 R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/OneDrive/Documentos/GitHub/SLA-VELOZZ/postcss.config.js_.loader.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
